f369df8c5f8cc73c557fcefe4d15dca69949a506
Anna Langner <annalangner06@gmail.com>
2020-02-19 15:16:58 +0100

    Adding UI vars

---
+++ app.js
@@ -0,0 +1,6 @@
+//UI vars
+const form = document.querySelector('#shoping-form');
+const shopingList = document.querySelector('.collection');
+const filterItem = document.querySelector('#filter');
+const clearBnt = document.querySelector('.clear-item');
+const shoppingInput = document.querySelector('#Item');
