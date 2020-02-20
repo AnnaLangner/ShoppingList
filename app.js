f75944af4dedb1864db18baf85b72d3d8545d739
Anna Langner <annalangner06@gmail.com>
2020-02-20 21:43:00 +0100

    Adding Event Listener and function Add Item

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
 //UI vars
-const form = document.querySelector('#shoping-form');
-const shopingList = document.querySelector('.collection');
+const form = document.querySelector('#shopping-form');
+const shoppingList = document.querySelector('.collection');
 const filterItem = document.querySelector('#filter');
 const clearBnt = document.querySelector('.clear-item');
-const shoppingInput = document.querySelector('#Item');
+const shoppingInput = document.querySelector('#item');
+
+//Add all eventListeners
+loadEventListeners();
+
+//Event Listeners
+function loadEventListeners() {
+  //Add item event
+  form.addEventListener('submit', addItem);
+}
+
+//Add item
+function addItem(e) {
+  if(shoppingInput.value === ''){
+    alert('Add item');
+  }
+
+  //Create li element
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  //Create text node and append it to li
+  li.appendChild(document.createTextNode(shoppingInput.value));
+  //Create a new link element
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  //Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  //Append the link to li
+  li.appendChild(link);
+  //Append li to ul
+  shoppingList.appendChild(li);
+
+  //Clear input
+  shoppingInput.value = '';
+
+  e.preventDefault();
+}
