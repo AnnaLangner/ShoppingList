43b87802fe53f86b8879d04849ed7c6ce3c7c6cb
Anna Langner <annalangner06@gmail.com>
2020-02-23 18:51:46 +0100

    Clearing code

--- app.js
+++ app.js
@@ -1,101 +1,47 @@
-//UI vars
 const form = document.querySelector('#shopping-form');
 const shoppingList = document.querySelector('.collection');
 const filter = document.querySelector('#filter');
 const clearBnt = document.querySelector('.clear-items');
 const shoppingInput = document.querySelector('#item');
 
-//Add all eventListeners
 loadEventListeners();
 
-//Event Listeners
 function loadEventListeners() {
-  //DOM load event
-  document.addEventListener('DOMContentLoaded', getItems);
-  //Add item event
+  document.addEventListener('DOMContentLoaded', getItemsFromLocalStorage);
   form.addEventListener('submit', addItem);
-  //Remove item
   shoppingList.addEventListener('click', removeItem);  
-  //Clear list
   clearBnt.addEventListener('click', clearList);
-  //Filter item
   filter.addEventListener('keyup', filterItems);
 }
 
-//Get Items from LocalStorage
-function getItems() {
-  let items;
-  if(localStorage.getItem('items') === null) {
-    items = [];
-  } else {
-    items = JSON.parse(localStorage.getItem('items'));
-  }
+function getItemsFromLocalStorage() {
+  const items = getItems();
 
   items.forEach(function(item) {
     const li = document.createElement('li');
     li.className = 'collection-item';
-    const key = item['name'];
-    li.appendChild(document.createTextNode(key));
-    const itemCounter = document.createElement('input');
-    itemCounter.className = 'number-of-items';
-    itemCounter.setAttribute("type", "number");
-    itemCounter.setAttribute("value", item['value']);
-    itemCounter.setAttribute("min", 1);
-    itemCounter.setAttribute('step', 1);  
-    li.appendChild(itemCounter);    
-    const link = document.createElement('a');
-    link.className = 'delete-item secondary-content';
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    li.appendChild(link);
-    const submit = document.createElement('a');
-    submit.className = 'submit-number secondary-content';
-    submit.innerHTML = '<i class="fa fa-save"></i>';
-    submit.addEventListener('click', submitNumber);
-    li.appendChild(submit);
+    const key = item.name;
+    li.appendChild(document.createTextNode(key));  
+    li.appendChild(createItemCounter(item.value));     
+    li.appendChild(createRemoveButton());
+    li.appendChild(createSaveButton());
     shoppingList.appendChild(li);
   });
 }
 
-//Add item
 function addItem(e) {
   if(shoppingInput.value === ''){
     alert('Add item');
   }
 
-  //Create li element
   const li = document.createElement('li');
   li.className = 'collection-item';
-  //Create text node and append it to li
   li.appendChild(document.createTextNode(shoppingInput.value));
-  //Create an element nuber of item
-  const itemCounter = document.createElement('input');
-  itemCounter.className = 'number-of-items';
-  itemCounter.setAttribute("type", "number");
-  itemCounter.setAttribute("value", 1);
-  itemCounter.setAttribute("min", 1);
-  itemCounter.setAttribute('step', 1);   
-  //Append the number to li
-  li.appendChild(itemCounter);  
-  //Create a new link element
-  const link = document.createElement('a');
-  link.className = 'delete-item secondary-content';
-  //Add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  //Append the link to li
-  li.appendChild(link);
-  //create a new submit elemet
-  const submit = document.createElement('a');
-  submit.className = 'submit-number secondary-content'
-   //Add icon html
-  submit.innerHTML = '<i class="fa fa-save"></i>';
-  //Submit changes number of items
-  submit.addEventListener('click', submitNumber);
-  //Append the submit to li
-  li.appendChild(submit);
-  //Append li to ul
+  li.appendChild(createItemCounter(1)); 
+  li.appendChild(createRemoveButton());
+  li.appendChild(createSaveButton());
   shoppingList.appendChild(li);
 
-  //Storage in LS
   storeItemInLocalStorage(shoppingInput.value, 1);
 
   //Clear input
@@ -104,42 +50,63 @@ function addItem(e) {
   e.preventDefault();
 }
 
-//Store Item
+function createItemCounter(value) {
+  const itemCounter = document.createElement('input');
+  itemCounter.className = 'number-of-items';
+  itemCounter.setAttribute("type", "number");
+  itemCounter.setAttribute("value", value);
+  itemCounter.setAttribute("min", 1);
+  itemCounter.setAttribute('step', 1); 
+  return itemCounter;
+}
+
+function createSaveButton() {
+  const saveButton = document.createElement('a');
+  saveButton.className = 'submit-number secondary-content'
+  saveButton.innerHTML = '<i class="fa fa-save"></i>';
+  saveButton.addEventListener('click', saveNumberOfItems);
+  return saveButton;
+}
+
+function createRemoveButton() {
+  const removeButton = document.createElement('a');
+  removeButton.className = 'delete-item secondary-content';
+  removeButton.innerHTML = '<i class="fa fa-remove"></i>';
+  return removeButton;
+}
+
 function storeItemInLocalStorage(item, amount) {
-  let items;
-  if(localStorage.getItem('items') === null) {
-    items = [];
-  } else {
-    items = JSON.parse(localStorage.getItem('items'));
-  }
+  const items = getItems();
   let entry = new Object;
-  entry["name"] = item;
-  entry["value"] = amount;
+  entry.name = item;
+  entry.value = amount;
   items.push(entry);
 
   localStorage.setItem('items', JSON.stringify(items));
 }
 
-//Remove Item
-function removeItem(e) {
-  if(e.target.parentElement.classList.contains('delete-item')) {
-    if(confirm('Are You Sure?')) {
-      e.target.parentElement.parentElement.remove();
-
-      //Remove from LS
-      removeItemFromLocalStorage(e.target.parentElement.parentElement);
-    }
-  }
-}
-
-//Remove from LS
-function removeItemFromLocalStorage(shoppingItem) {
+function getItems() {
   let items;
   if(localStorage.getItem('items') === null) {
     items = [];
   } else {
     items = JSON.parse(localStorage.getItem('items'));
   }
+  return items;
+}
+
+function removeItem(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();
+
+      removeItemFromLocalStorage(e.target.parentElement.parentElement);
+    }
+  }
+}
+
+function removeItemFromLocalStorage(shoppingItem) {
+  const items = getItems();
 
   items.forEach(function(item, index) {
     if(shoppingItem.textContent === item.name) {
@@ -150,37 +117,28 @@ function removeItemFromLocalStorage(shoppingItem) {
   localStorage.setItem('items', JSON.stringify(items));
 }
 
-//Submit changes number of item
-function submitNumber(e) {
+function saveNumberOfItems(e) {
   const key = e.target.parentElement.parentElement.childNodes[0].textContent;
   const value = e.target.parentElement.parentElement.childNodes[1].value;
-  let items;
-  if(localStorage.getItem('items') === null) {
-    items = [];
-  } else {
-    items = JSON.parse(localStorage.getItem('items'));
-  }
-  items.find(item => item['name'] === key)['value'] = value;
+  const items = getItems();
+
+  items.find(item => item.name === key).value = value;
 
   localStorage.setItem('items', JSON.stringify(items));
 }
 
-//Clear list
 function clearList(e) {
   while(shoppingList.firstChild) {
     shoppingList.removeChild(shoppingList.firstChild);
   }
 
-  //clear from LS
   clearListFromLocalStorage();
 }
 
-//Clear from LS
 function clearListFromLocalStorage() {
   localStorage.clear();
 }
 
-//Filter items
 function filterItems(e) {
   const text = e.target.value.toLowerCase();
 
